bc146e21666b4be52bf598903efe5ea3db47213c
mallowigi <heliosaian@gmail.com>
2015-04-25 11:50:08 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+var atom  = require('gulp-atom');
+
+// Build project
+gulp.task('build', function() {
+  return atom({
+      srcPath: './src',
+      releasePath: './release',
+      cachePath: './cache',
+      version: 'v0.12.4',
+      rebuild: true,
+      platforms: ['osx']
+  });
+})
+
+//Run project
+gulp.task('run', shell.task([
+  './release/v0.12.4/darwin/Atom.app/Contents/MacOS/Atom src/'
+]))
+
+gulp.task('default', ['atom']);
